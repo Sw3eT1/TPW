a7e8bbe9b0cbde671d65358a8738849fedd4f82d
Rafal251566 <251566@edu.p.lodz.pl>
2025-03-24 18:30:34 +0100

    Dodano testy jednostkowe do Dane i Logika

---
+++ UnitTests/DaneTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+using Dane;
+using NUnit.Framework.Legacy;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class BallDataTests
+    {
+        [Test]
+        public void BallData_SetAndGetTests()
+        {
+            BallData ballData = new BallData();
+            double x = 10;
+            double y = 20;
+            double radius = 30;
+            double movX = 4;
+            double movY = -5;
+
+            ballData.X = x;
+            ballData.Y = y;
+            ballData.Radius = radius;
+            ballData.MovX = movX;
+            ballData.MovY = movY;
+
+            Assert.That(ballData.X, Is.EqualTo(x));
+            Assert.That(ballData.Y, Is.EqualTo(y));
+            Assert.That(radius, Is.EqualTo(ballData.Radius));
+            Assert.That(movX, Is.EqualTo(ballData.MovX));
+            Assert.That(movY, Is.EqualTo(ballData.MovY));
+        }
+    }
+}
---
+++ UnitTests/LogikaTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+using Logika;
+using NUnit.Framework.Legacy;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class LogikaTest
+    {
+        private Random random;
+        private int maxWidth;
+        private int maxHeight;
+
+        [SetUp]
+        public void Setup()
+        {
+            random = new Random(1);
+            maxWidth = 100;
+            maxHeight = 100;
+        }
+
+        [Test]
+        public void BallLogic_ConstructorTests()
+        {
+            BallLogic ballLogic = new BallLogic(random, maxWidth, maxHeight);
+
+            ClassicAssert.IsNotNull(ballLogic.Data);
+            ClassicAssert.IsTrue(ballLogic.Data.X >= 10 && ballLogic.Data.X <= maxWidth);
+            ClassicAssert.IsTrue(ballLogic.Data.Y >= 10 && ballLogic.Data.Y <= maxHeight);
+            ClassicAssert.IsTrue(ballLogic.Data.Radius >= 20 && ballLogic.Data.Radius <= 50);
+            ClassicAssert.IsTrue(ballLogic.Data.MovX >= -5 && ballLogic.Data.MovX <= 5);
+            ClassicAssert.IsTrue(ballLogic.Data.MovY >= -5 && ballLogic.Data.MovY <= 5);
+            ClassicAssert.IsNotNull(ballLogic.Data.Color);
+        }
+    }
+}
